1af9db0e548132e4bbd65252c0dcbf6544810dc5
RyanSquared <vandor2012@gmail.com>
2020-05-26 04:59:11 -0500

    terraform: fix outputting correct IAM access key

--- terraform/modules/r53/outputs.tf
+++ terraform/modules/r53/outputs.tf
@@ -1,5 +1,5 @@
 output "kubernetes-cert-manager-dns-iam-user" {
-  value = aws_iam_user.kubernetes-cert-manager-dns.name
+  value = aws_iam_access_key.kubernetes-cert-manager-dns.id
 }
 
 output "kubernetes-cert-manager-dns-iam-key" {
